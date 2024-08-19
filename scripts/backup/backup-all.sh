c059d58ec6c777d6bd9330bc2f8a25007c24a799
Jake Farrell <jake_farrell@outlook.com>
2024-08-19 14:18:28 +0000

    added tcv app, added postgres

---
+++ scripts/backup/backup-all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/home/debian/scripts/backup/backup-fotosoc-wordpress-database.sh
+/home/debian/scripts/backup/backup-plausible.sh
+/home/debian/scripts/backup/backup-shlink.sh
+/home/debian/scripts/backup/backup-the-experiment.sh
+/home/debian/scripts/backup/backup-zipline.sh
