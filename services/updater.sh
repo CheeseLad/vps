eb9d5ee3b2bfe3d351d1358f9cf67c9d293b42bf
Jake Farrell <jake_farrell@outlook.com>
2025-11-29 02:57:15 +0000

    add docker updater, add listmonk, remove hostname flag

--- services/updater.sh
+++ services/updater.sh
@@ -3,15 +3,16 @@
 # pull any new images and
 # recreate the containers if a new image is pulled
 
-now=`date +"%Y-%m-%d %T"`
+now=$(date +"%Y-%m-%d %T")
 printf "%s\n---\n" "$now"
-for dir in ./*; do 
-    if [[ -d "$dir" && ${dir} != *"disabled"* ]]; then
-        cd "$dir"
-        /usr/bin/docker compose pull -q
+for dir in /home/debian/services/*; do 
+    if [[ -d "$dir" && "${dir}" != *"disabled"* ]]; then
+         cd "$dir" || continue
+        /usr/bin/docker compose pull
         /usr/bin/docker compose up -d
         cd ..
     fi
 done
+
 docker image prune -f
 echo
